954daa3e427304feddd83a97ce936387ef07ab72
venkatesh <venkateshkumar.chdec@gmail.com>
2024-03-30 22:50:22 -0500

    New.py has added some code

---
+++ new.py
@@ -0,0 +1,30 @@
+import random
+
+def generate_random_list(length):
+    return [random.randint(1, 1000) for _ in range(length)]
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+def print_list(arr):
+    for num in arr:
+        print(num)
+
+def main():
+    length = 20
+    random_list = generate_random_list(length)
+
+    print("Unsorted List:")
+    print_list(random_list)
+
+    bubble_sort(random_list)
+
+    print("\nSorted List:")
+    print_list(random_list)
+
+if __name__ == "__main__":
+    main()
